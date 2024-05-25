2934e2bd23740d66c1d985a4ca20f7c314c4ce6e
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-05-25 12:55:06 -0700

    Initialized MCQ's JavaScript

---
+++ app.js
@@ -0,0 +1,14 @@
+// Chatgpt JavaScript complex MCQ's (20)
+
+// Question 01 : Which of the following will output 'true'?
+// const obj = {
+//     a: 'foo',
+//     b: {
+//         c: 'bar'
+//     }
+// };
+
+// const {a, b: { c }} = obj;
+// console.log(a === 'foo' && c === 'bar');
+
+// a) 'true'  b) 'false'  c) 'undefined'  d) 'TypeError'
